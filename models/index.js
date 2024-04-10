e9b785369d23a3f81f7001e914747a51d654a7bf
Nate Johnson <njohnson2897@gmail.com>
2024-04-10 16:01:09 -0500

    adds work from tutoring session

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const BlogPost = require('./BlogPost');
+const Comment = require('./Comment')
 
 User.hasMany(BlogPost, {
   foreignKey: 'user_id',
@@ -10,4 +11,16 @@ BlogPost.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, BlogPost };
+// User.hasMany(Comment)
+
+// BlogPost.hasMany(Comment)
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(BlogPost, {
+  foreignKey: 'blog_id'
+});
+
+module.exports = { User, BlogPost, Comment };
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,21 +1,20 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#project-name').value.trim();
-    const needed_funding = document.querySelector('#project-funding').value.trim();
-    const description = document.querySelector('#project-desc').value.trim();
+    const title = document.querySelector('#project-name').value.trim();
+    const content = document.querySelector('#project-desc').value.trim();
   
-    if (name && needed_funding && description) {
-      const response = await fetch(`/api/projects`, {
+    if (title && content) {
+      const response = await fetch(`/api/posts`, {
         method: 'POST',
-        body: JSON.stringify({ name, needed_funding, description }),
+        body: JSON.stringify({ title, content }),
         headers: {
           'Content-Type': 'application/json',
         },
       });
   
       if (response.ok) {
-        document.location.replace('/profile');
+        document.location.replace('/dashboard');
       } else {
         alert('Failed to create project');
       }
@@ -31,7 +30,7 @@ const newFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/profile');
+        document.location.replace('/dashboard');
       } else {
         alert('Failed to delete project');
       }
